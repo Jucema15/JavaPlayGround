8a5ea2245950ef75785f46437b6d0784c781bb0f
Jucema15 <jmantilla23988@gmail.com>
2026-03-02 16:02:53 -0500

    Primer dia

---
+++ IntroduccionJava/src/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Soy Julio Cesar Mantilla");
+        System.out.println("Tengo 27 años");
+        System.out.println("Soy de Colombia");
+    }
+}
---
+++ Variables/src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Variables
+        int edad = 31;
+        double precio = 300.40;
+        boolean disponible = true;
+        char genero;
+        genero = 'M';
+
+        // Acceder a las variables
+        System.out.println(edad);
+        System.out.println(precio);
+        System.out.println(disponible);
+        System.out.println(genero);
+    }
+}
