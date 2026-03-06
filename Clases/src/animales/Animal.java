d3e50f1c770c2ed5f20ea846a586d5452b8228dc
Jucema15 <jmantilla23988@gmail.com>
2026-03-06 11:57:19 -0500

    Día 4 practica

--- Clases/src/animales/Animal.java
+++ Clases/src/animales/Animal.java
@@ -1,32 +1,41 @@
 package animales;
 
 public class Animal {
-    protected void comer(){
-        System.out.println("Como muchas veces al día");
-    }
-    protected void dormir(){
-        System.out.println("Duermo muchas horas");
+    protected void hacerSonido(){
+        System.out.println("El animal hace un sonido");
     }
 }
 
 class Perro extends Animal{
-    public void hacerSonido(){
-        System.out.println("Puedo ladrar");
+    @Override
+    protected void hacerSonido(){
+        System.out.println("El perro hace wauf");
+    }
+}
+
+class Gato extends Animal{
+    @Override
+    protected void hacerSonido(){
+        System.out.println("El gato hace miau");
     }
 }
 
 class PruebaAnimal{
-    public static void main(String[] args) {
-        System.out.println("*** Ejemplo de herencia ***");
-        System.out.println("Clase Padre. Soy un animal");
-        var animal1 = new Animal();
-        animal1.comer();
-        animal1.dormir();
 
-        System.out.println("\nClase hija. Soy un perro");
-        var perro1 = new Perro();
-        perro1.comer();
-        perro1.dormir();
-        perro1.hacerSonido();
+    // Método polimorfico
+    static void imprimirSonido(Animal animal){
+        animal.hacerSonido();
+    }
+
+    public static void main(String[] args) {
+        //Objeto de la clase Padre (Animal)
+        var animal = new Animal();
+        imprimirSonido(animal);
+        var perro = new Perro();
+        System.out.println();
+        imprimirSonido(perro);
+        var gato = new Gato();
+        System.out.println();
+        imprimirSonido(gato);
     }
 }
---
+++ TemasAvanzados/src/ArgumentosVariables.java
@@ -0,0 +1,20 @@
+public class ArgumentosVariables {
+
+    public static void main(String[] args) {
+
+        //imprimirNumeros(1,2,3,4,5); // var arg
+
+        variosParametros("karla", 10, 20, 30);
+    }
+
+    static void variosParametros(String nombre, int... numeros) {
+        System.out.println("Nombre: " + nombre);
+        imprimirNumeros(numeros);
+    }
+
+    static void imprimirNumeros(int... numeros) {
+        for(var i = 0; i < numeros.length; i++){
+            System.out.println(numeros[i] + " ");
+        }
+    }
+}
