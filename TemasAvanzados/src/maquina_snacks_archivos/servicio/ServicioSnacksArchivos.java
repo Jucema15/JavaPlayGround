018a551a8a16534a17a6609ee719e3335e5f4d88
Jucema15 <jmantilla23988@gmail.com>
2026-03-17 11:49:22 -0500

    Día 7 Temas avanzados

---
+++ TemasAvanzados/src/maquina_snacks_archivos/servicio/ServicioSnacksArchivos.java
@@ -0,0 +1,96 @@
+package maquina_snacks_archivos.servicio;
+
+import maquina_snacks_archivos.dominio.Snack;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicioSnacksArchivos implements IServicioSnacks {
+    private final String NOMBRE_ARCHIVO = "snacks.txt";
+    private List<Snack> snacks = new ArrayList<>();
+
+    public ServicioSnacksArchivos() {
+        var archivo = new File(NOMBRE_ARCHIVO);
+        var existe = false;
+        try {
+            existe = archivo.exists();
+            if (existe) {
+                //this.snacks = obtenerSnacks();
+            } else {
+                var salida = new PrintWriter(new FileWriter(archivo));
+                salida.close();
+                System.out.println("Se ha creado el archivo");
+            }
+        } catch (Exception e) {
+            System.out.println("Error al crear el archivo " + e.getMessage());
+        }
+
+        if (!existe) {
+            cargarSnacksIniciales();
+        }
+    }
+
+    private void cargarSnacksIniciales(){
+        this.agregarSnack(new Snack("Papas", 70));
+        this.agregarSnack(new Snack("Refresco", 50));
+        this.agregarSnack(new Snack("Sandwich", 120));
+    }
+
+    @Override
+    public void agregarSnack(Snack snack) {
+        this.snacks.add(snack);
+        this. agregarSnackArchivo(snack);
+    }
+
+    private void agregarSnackArchivo(Snack snack){
+        boolean anexar = false;
+        var archivo = new File(NOMBRE_ARCHIVO);
+        try {
+            anexar = archivo.exists();
+            var salida = new PrintWriter(new FileWriter(archivo, anexar));
+            salida.println(snack.escribirSnack());
+            salida.close();
+        } catch (Exception e) {
+            System.out.println("Hubo un error al agregar snack " + e.getMessage());
+        }
+    }
+
+    private List<Snack> obtenerSnacks(){
+        var snacks = new ArrayList<Snack>();
+        try {
+            List<String> lineas = Files.readAllLines(Paths.get(NOMBRE_ARCHIVO));
+            for(String linea: lineas){
+                String[] lineaSnack = linea.split(",");
+                var idSnack = lineaSnack[0];
+                var nombre = lineaSnack[1];
+                var precio = Double.parseDouble(lineaSnack[2]);
+                var snack = new Snack(nombre, precio);
+                snacks.add(snack);
+            }
+        } catch (Exception e) {
+            System.out.println("Error al leer el archivo de snacks: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return snacks;
+    }
+
+    @Override
+    public void mostrarSnacks() {
+        System.out.println("--- Snacks en el Inventario");
+        var inventarioSnacks = "";
+        for(var snack: this.snacks){
+            inventarioSnacks += snack.toString() + "\n";
+        }
+        System.out.println(inventarioSnacks);
+    }
+
+    @Override
+    public List<Snack> getSnacks() {
+        return List.of();
+    }
+}
