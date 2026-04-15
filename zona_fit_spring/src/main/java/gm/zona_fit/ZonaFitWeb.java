0074bf0a0ffc832fd97945ad9520f742e927205c
Jucema15 <jmantilla23988@gmail.com>
2026-04-15 14:56:46 -0500

    Día 12 Zona Fit Web

---
+++ zona_fit_spring/src/main/java/gm/zona_fit/ZonaFitWeb.java
@@ -0,0 +1,8 @@
+package gm.zona_fit
+
+@SpringBootApplication
+public class ZonaFitWeb{
+    public static void main(String[] args) {
+        SpringApplication.run(ZonaFitWeb.class, args);
+    }
+}
