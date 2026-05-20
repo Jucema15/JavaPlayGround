93164b1efd375522656238a6a8b620217606c9f9
Jucema15 <jmantilla23988@gmail.com>
2026-05-20 11:24:47 -0500

    Finalización del proyecto ZonaFitt

--- ZonaFitSpring/src/main/java/gm/zona_fit/controlador/IndexControlador.java
+++ ZonaFitSpring/src/main/java/gm/zona_fit/controlador/IndexControlador.java
@@ -3,8 +3,11 @@ package gm.zona_fit.controlador;
 import gm.zona_fit.modelo.Cliente;
 import gm.zona_fit.servicio.IClienteServicio;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,7 @@ public class IndexControlador {
     IClienteServicio clienteServicio;
     private List<Cliente> clientes;
     private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
+    private Cliente clienteSeleccionado;
 
     @PostConstruct
     public void init(){
@@ -30,6 +34,42 @@ public class IndexControlador {
     public void cargarDatos() {
         this.clientes = this.clienteServicio.listarClientes();
         this.clientes.forEach(cliente -> logger.info(cliente.toString()));
+    }
+
+    public void agregarCliente(){
+        this.clienteSeleccionado = new Cliente();
+    }
+
+    public void guardarCliente(){
+        logger.info("Cliente a guardar:" + this.clienteSeleccionado);
+        if(this.clienteSeleccionado.getId() == null){
+            this.clienteServicio.guardarCliente(this.clienteSeleccionado);
+            this.clientes.add(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Cliente Agregado"));
+        }else{
+            this.clienteServicio.guardarCliente(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cliente Actulizado"));
+        }
+
+        PrimeFaces.current().executeScript("PF('ventanaModalCliente').hide()");
+
+        PrimeFaces.current().ajax().update("forma-clientes:mensajes",
+        "forma-clientes:clientes-tabla");
+
+        this.clienteSeleccionado = null;
+    }
+
+    public void eliminarCliente(){
+        logger.info("Cliente a eliminar: " + this.clienteSeleccionado);
+        this.clienteServicio.eliminarCliente(this.clienteSeleccionado);
+
+        this.clientes.remove(this.clienteSeleccionado);
+        this.clienteSeleccionado = null;
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Cliente Eliminado"));
+        PrimeFaces.current().ajax().update("forma-clientes:mensajes",
+                "forma-clientes:clientes-tabla");
     }
 
 }
